ccc4b209840c5a8df6dbc0f8bf2cc2a682746f0b
kmameng <hardymameng@163.com>
2024-09-17 10:54:39 +0800

    Update file2.py

--- 马猛/week2/file2.py
+++ 马猛/week2/file2.py
@@ -3,12 +3,11 @@ from skimage.color import rgb2gray
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 plt.subplot(221)
 plt.imshow(plt.imread("lenna.png"))
 
 img = cv2.imread("lenna.png")
-height, width = img.shape[:2]
+height, width, k = img.shape[:3]
 img_gray = np.zeros([height, width], img.dtype)
 
 for i in range(height):
@@ -26,21 +25,20 @@ plt.imshow(img_gray, cmap='gray')
 # cv2.waitKey(0)
 print(img_gray)
 
-img_gray = rgb2gray(img)
+img_gray = rgb2gray(plt.imread("lenna.png"))
 
 print(img_gray)
 
 hang, shu = img_gray.shape
 for i in range(hang):
     for j in range(shu):
-        if (img_gray[i, j] > 0.5):
+        if img_gray[i, j] > 0.5:
             img_gray[i, j] = 1
         else:
             img_gray[i, j] = 0
-print(img_gray)
 # img_gray = np.where(img_gray > 0.5, 1, 0)
+print(img_gray)
 
 plt.subplot(223)
 plt.imshow(img_gray, cmap='gray')
 plt.show()
-
